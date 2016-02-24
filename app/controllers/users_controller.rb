10f4b0c1dd9133d3c591c2e97e890fabd648ea11
Ryan Newell <youcatastrophe@gmail.com>
2016-02-24 12:19:23 -0600

    CREATE action for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,13 @@
-# This controller is for all the CRUD operations related to a User.
+MyApp.get "/users/new" do 
+  erb :"users/new"
+end 
+
+MyApp.post "/users/create" do 
+  @user = User.new
+  @user.name = params["name"]
+  @user.email = params["email "]
+  @user.password = params["password"]
+  @user.save
+
+  erb :"users/created"
+end 
