157cfae7565a092256cd515002b4ccdbde0d354d
Ryan Newell <youcatastrophe@gmail.com>
2016-02-25 15:19:47 -0600

    Slowly updating User controllers to reflect new naming convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,23 +22,13 @@ MyApp.get "/users/:id" do
   erb :"users/show"
 end
 
-MyApp.get "/users/view_user/:id" do
-  @current_user = User.find_by_id(session["user_id"])
 
-  if @current_user != nil 
-    @user = User.find_by_id(params[:id])
-    erb :"users/view_user"
-  else 
-    erb :"login_first"
-  end
-end
-
-MyApp.get "/users/edit/:id" do
+MyApp.get "/users/:id/edit" do
   @user = User.find_by_id(params[:id])
   erb :"users/edit"
 end
 
-MyApp.post "/users/process_edit/:id" do
+MyApp.post "/users/:id/update" do
   @current_user = User.find_by_id(session["user_id"])
 
   if @current_user != nil 
@@ -47,19 +37,15 @@ MyApp.post "/users/process_edit/:id" do
     @user.email = params["new_email"]
     @user.password = params["new_password"]
     @user.save
-    erb :"users/update_successful"
+
+  redirect "/users/#{user.id}"
   else
     erb :"login_first"
   end 
 end 
 
-MyApp.get "/users/delete/:id" do
-  @user = User.find_by_id(params[:id])
-  erb :"users/delete"
-end 
-
-MyApp.post "/users/process_delete/:id" do
+MyApp.post "/users/:id/delete/" do
   @user = User.find_by_id(params[:id])
   @user.delete
-  erb :"users/update_successful"
+  redirect "/"
 end 
