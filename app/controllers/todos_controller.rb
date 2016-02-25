c96c29184b3c2983d965c243d344e5f7f3ce099a
Ryan Newell <youcatastrophe@gmail.com>
2016-02-25 07:02:11 -0600

    finished CREATE action for Todos.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,30 +10,20 @@ end
 
 
 MyApp.post "/todos/create" do 
-  @current_user = User.find_by_id(session["user_id"])
 
-  if @current_user != nil   
+    @new_todo = Todo.new
     @new_todo.completed = false
-    @new_todo.name = params["title"]
+    @new_todo.title = params["title"]
     @new_todo.description = params["description"]
     @new_todo.user_id = session["user_id"]
     @new_todo.save
     erb :"todos/created"
-  else  
-    erb :"login_first"
-  end
 end 
 
 
 MyApp.get "/todos/view_todo/:id" do
-  @current_user = User.find_by_id(session["user_id"])
-
-  if @current_todo != nil 
     @todo = Todo.find_by_id(params[:id])
     erb :"todos/view_todo"
-  else 
-    erb :"login_first"
-  end
 end
 
 MyApp.get "/todos/edit/:id" do
