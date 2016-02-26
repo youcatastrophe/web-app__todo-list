f257c975af11e6efbbe0413208c0e71d18cce21f
Ryan Newell <youcatastrophe@gmail.com>
2016-02-25 20:41:07 -0600

    Most likely done for the evening.

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+  end
+
   create_table "todos", force: :cascade do |t|
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
@@ -23,6 +29,7 @@ ActiveRecord::Schema.define(version: 0) do
     t.boolean  "completed"
     t.integer  "user_id"
     t.integer  "assigned_to_id"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
